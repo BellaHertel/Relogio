b61138da24f0422ec9b6204fc532d6a81910f1fd
Isabella Hertel <isabellalunnaferraz@gmail.com>
2024-01-17 23:28:29 -0300

    Primeiro commit!

---
+++ index.js
@@ -0,0 +1,37 @@
+// Carregando variávels DOM
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second"),
+  modeSwitch = document.querySelector(".mode-switch");
+
+// Checar se está em DarkMode
+if (localStorage.getItem("mode") === "Dark Mode") {
+  body.classList.add("dark");
+  modeSwitch.textContent = "Light Mode";
+}
+
+// Evento de Click para trocar os temas
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  const isDarkMode = body.classList.contains("dark");
+  modeSwitch.textContent = isDarkMode ? "Light Mode" : "Dark Mode";
+  localStorage.setItem("mode", isDarkMode ? "Dark Mode" : "Light Mode");
+});
+
+// Atualizar Horário
+const updateTime = () => {
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+  // Rodar os Ponteiros
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+// Relógio roda a cada segundo
+setInterval(updateTime, 1000);
+updateTime();
